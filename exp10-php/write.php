652bd618e28b3d3b9daaa6254745737556a5c51e
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-08-04 08:20:38 +0530

    improved styles

--- exp10-php/write.php
+++ exp10-php/write.php
@@ -1,5 +1,4 @@
 <?php
-echo implode(', ', $_POST);
 $a = $_POST['userid'];
 $b = $_POST["username"];
 $c = $_POST["product"];
@@ -18,20 +17,4 @@ $newFb->setAttribute('fb', $d);
 
 $global->appendChild($newFb);
 $xml->save("form.xml");
-
-// $item0 = $xml->createElement("User");
-// $item1 = $xml->createElement("UserID");
-// $item2 = $xml->createElement("UserName");
-// $item3 = $xml->createElement("Product");
-// $item4 = $xml->createElement("FeedBack");
-
-// $item0->appendChild($item1);
-// $item0->appendChild($item2);
-// $item0->appendChild($item3);
-// $item0->appendChild($item4);
-// $item1->nodeValue = $a;
-// $item2->nodeValue = $b;
-// $item3->nodeValue = $c;
-// $item4->nodeValue = $d;
-
-//echo $xml->save("form.xml");
+echo "<div style=\"height: 100vh; width: 100%; display: flex; align-items: center; justify-content: center; font-size: 4rem\">Feedback added successfully.</div>";
