a78d0b5c723275b18d224b63e8c041478fb136c1
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-08-09 11:14:43 +0530

    (feat): added validation

--- reg-servlet/src/HelloForm.java
+++ reg-servlet/src/HelloForm.java
@@ -16,10 +16,28 @@ public class HelloForm extends HttpServlet {
 		String title = "Using GET Method to Read Form Data";
 		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
 
+		String fName = request.getParameter("first_name");
+		String lName = request.getParameter("last_name");
 		String name = request.getParameter("first_name") + " " + request.getParameter("last_name");
 		String email = request.getParameter("email");
 		String phone = request.getParameter("phone");
 		String address = request.getParameter("address");
+
+		if (fName.trim().equals("") || lName.trim().equals("")) {
+			out.println("Name can't be empty");
+		}
+
+		if (email.equals("")) {
+			out.println("Email can't be empty");
+		}
+
+		if (phone.equals("")) {
+			out.println("Phone number can't be empty");
+		}
+
+		if (address.equals("")) {
+			out.println("Address can't be empty");
+		}
 
 		String htmlString = "<html><head><title>" + title + "</title></head><body bgcolor=\"#f0f0f0\"><h1 align=\"center\">"
 				+ title + "</h1><div style=\"text-align:center; font-size: 22px;\">";
